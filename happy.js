74e940a6e536f4da060c754f207dd3e11f3c007a
Balaji Asish <balajiashish02@gmail.com>
2025-03-30 22:33:35 +0530

    Update happy.js

--- happy.js
+++ happy.js
@@ -4,3 +4,4 @@ fetch('https://jsonplaceholder.typicode.com/posts/1')
   .then(response => response.json()) // Parse the response as JSON
   .then(data => console.log(data))   // Handle the data
   .catch(error => console.error('Error:', error)); // Handle errors
+dfdasfa
